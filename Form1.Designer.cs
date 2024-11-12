c9fa2869d592fb9bb8c5c44cb18c1afc7b64fff3
Thomas Ragen <thomas95ragen@gmail.com>
2024-11-12 15:07:15 +0300

    Created an error logging mechanism for upload and generation/remaining conversion Updated button generate to deactivate when no file is uploaded and after file is generated

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,12 @@
-﻿namespace exceml
+﻿using ExcelDataReader;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System;
+
+namespace exceml
 {
     partial class Form1
     {
@@ -15,49 +23,70 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.btnUpload = new System.Windows.Forms.Button();
             this.btnGenerateXML = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
             // label1
+            // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 22);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 20);
+            this.label1.Size = new System.Drawing.Size(278, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select Excel File to Convert";
-
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // btnUpload
-            this.btnUpload.Location = new System.Drawing.Point(157, 77);
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnUpload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpload.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.ForeColor = System.Drawing.Color.White;
+            this.btnUpload.Location = new System.Drawing.Point(170, 80);
             this.btnUpload.Name = "btnUpload";
-            this.btnUpload.Size = new System.Drawing.Size(223, 42);
+            this.btnUpload.Size = new System.Drawing.Size(230, 50);
             this.btnUpload.TabIndex = 1;
             this.btnUpload.Text = "Upload Excel File";
-            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.UseVisualStyleBackColor = false;
             this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
-
+            // 
             // btnGenerateXML
-            this.btnGenerateXML.Location = new System.Drawing.Point(157, 182);
+            // 
+            this.btnGenerateXML.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnGenerateXML.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGenerateXML.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerateXML.ForeColor = System.Drawing.Color.White;
+            this.btnGenerateXML.Location = new System.Drawing.Point(170, 200);
             this.btnGenerateXML.Name = "btnGenerateXML";
-            this.btnGenerateXML.Size = new System.Drawing.Size(223, 42);
+            this.btnGenerateXML.Size = new System.Drawing.Size(230, 50);
             this.btnGenerateXML.TabIndex = 2;
             this.btnGenerateXML.Text = "Generate XML File";
-            this.btnGenerateXML.UseVisualStyleBackColor = true;
+            this.btnGenerateXML.UseVisualStyleBackColor = false;
             this.btnGenerateXML.Click += new System.EventHandler(this.btnGenerateXML_Click);
-
+            // 
             // Form1
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 308);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(580, 330);
             this.Controls.Add(this.btnGenerateXML);
             this.Controls.Add(this.btnUpload);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Excel to XML File Converter";
+            this.Text = "Exceml";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.Label label1;
@@ -65,3 +94,4 @@
         private System.Windows.Forms.Button btnGenerateXML;
     }
 }
+
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using System.Xml;
-using ExcelDataReader; 
+using ExcelDataReader;
 
 namespace exceml
 {
@@ -16,82 +17,227 @@ namespace exceml
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e) { }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            btnGenerateXML.Enabled = false; // Disable Generate button until file is uploaded
+            btnGenerateXML.Enabled = false;
         }
 
         private void btnUpload_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            try
             {
-                openFileDialog.Filter = "Excel Files|*.xls;*.xlsx";
-                openFileDialog.Title = "Select an Excel File";
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
                 {
-                    using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
-                    {
-                        using (var reader = ExcelReaderFactory.CreateReader(stream))
-                        {
-                            excelData = reader.AsDataSet(new ExcelDataSetConfiguration()
-                            {
-                                ConfigureDataTable = _ => new ExcelDataTableConfiguration() { UseHeaderRow = true }
-                            });
-                        }
-                    }
+                    openFileDialog.Filter = "Excel Files|*.xls;*.xlsx";
+                    openFileDialog.Title = "Select an Excel File";
 
-                    btnGenerateXML.Enabled = true; // Enable Generate XML button after file upload
-                    MessageBox.Show("Excel file loaded successfully.");
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                        {
+                            using (var reader = ExcelReaderFactory.CreateReader(stream))
+                            {
+                                excelData = reader.AsDataSet(new ExcelDataSetConfiguration()
+                                {
+                                    ConfigureDataTable = _ => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                                });
+                            }
+                        }
+
+                        btnGenerateXML.Enabled = true;
+                        MessageBox.Show("Excel file loaded successfully.");
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                LogError("Error uploading Excel file.", ex);
+                MessageBox.Show("An error occurred while uploading the file. Check error log for details.");
+            }
         }
+
 
         private void btnGenerateXML_Click(object sender, EventArgs e)
         {
-            if (excelData == null || excelData.Tables.Count == 0)
+            try
             {
-                MessageBox.Show("No data found in the Excel file.");
-                return;
-            }
-
-            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
-            {
-                saveFileDialog.Filter = "XML Files|*.xml";
-                saveFileDialog.Title = "Save XML File";
-
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                if (excelData == null || excelData.Tables.Count == 0)
                 {
-                    ConvertToXml(saveFileDialog.FileName);
-                    MessageBox.Show("XML file generated successfully.");
+                    MessageBox.Show("No data found in the Excel file.");
+                    return;
                 }
+
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "XML Files|*.xml";
+                    saveFileDialog.Title = "Save XML File";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        ConvertToXml(saveFileDialog.FileName);
+                        MessageBox.Show("XML file generated successfully.");
+                        btnGenerateXML.Enabled = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError("Error generating XML file.", ex);
+                MessageBox.Show("An error occurred while generating the XML file. Check error log for details.");
             }
         }
 
+
         private void ConvertToXml(string filePath)
         {
-            DataTable dataTable = excelData.Tables[0]; // Get the first sheet
-            using (XmlWriter writer = XmlWriter.Create(filePath, new XmlWriterSettings { Indent = true }))
+            DataTable dataTable = excelData.Tables[0];
+
+            using (XmlWriter writer = XmlWriter.Create(filePath, new XmlWriterSettings { Indent = true, IndentChars = "  " }))
             {
                 writer.WriteStartDocument();
-                writer.WriteStartElement("Workbook");
+                writer.WriteStartElement("transactions");
 
                 foreach (DataRow row in dataTable.Rows)
                 {
-                    writer.WriteStartElement("Row");
+                    writer.WriteStartElement("transaction");
 
-                    foreach (DataColumn column in dataTable.Columns)
-                    {
-                        writer.WriteStartElement(column.ColumnName);
-                        writer.WriteString(row[column].ToString());
-                        writer.WriteEndElement();
-                    }
+                    WriteElementIfNotEmpty(writer, "transactionnumber", ValidateString(row["Trans Reference"]));
+                    WriteElementIfNotEmpty(writer, "transaction_description", FormatDescription(ValidateString(row["TRANS DESCRIPTION"])));
+                    WriteElementIfNotEmpty(writer, "date_transaction", FormatDate(row["TRANSACTION DATE"]));
+                    WriteElementIfNotEmpty(writer, "amount_local", ValidateString(row["AMOUNT LCY"]));
+
+                    writer.WriteStartElement("t_from_my_client");
+                    WriteElementIfNotEmpty(writer, "from_funds_code", ValidateEnumeration(row["TRANS DESCRIPTION"], new[] { "AAT", "BC01", "BST", "C", "CD1", "CDFC", "CDOB", "COB", "CTBB", "CW1", "CWFC", "E", "H", "IFM", "INT", "INTC", "LP", "MM", "MR", "OCCD", "OESP", "OFM", "PCC", "STO", "TFA", "TFDB", "TTA", "TTDB" }));
+
+                    writer.WriteStartElement("from_person");
+                    WriteElementIfNotEmpty(writer, "gender", MapGender(row["GENDER"]?.ToString()));
+                    (string firstName, string lastName) = SplitName(ValidateString(row["TRANSACTING PERSON"]));
+                    WriteElementIfNotEmpty(writer, "first_name", firstName);
+                    WriteElementIfNotEmpty(writer, "last_name", lastName);
+                    WriteElementIfNotEmpty(writer, "birthdate", FormatDate(row["CONDUCTOR DOB"]));
+                    WriteElementIfNotEmpty(writer, "id_number", ValidateString(row["CONDUCTOR ID NUMBER"]));
+                    WriteElementIfNotEmpty(writer, "nationality1", ValidateString(row["CONDUCTOR NATIONALITY"]));
+                    WriteElementIfNotEmpty(writer, "occupation", ValidateString(row["CONDUCTOR OCCUPATION"]));
+                    writer.WriteEndElement();
+
+                    WriteElementIfNotEmpty(writer, "from_country", ValidateString(row["SIGNATORY NATIONALITY"]));
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("t_to_my_client");
+                    WriteElementIfNotEmpty(writer, "to_funds_code", ValidateEnumeration(row["TRANS DESCRIPTION"], new[] { "AAT", "BC01", "BST", "C", "CD1", "CDFC", "CDOB", "COB", "CTBB", "CW1", "CWFC", "E", "H", "IFM", "INT", "INTC", "LP", "MM", "MR", "OCCD", "OESP", "OFM", "PCC", "STO", "TFA", "TFDB", "TTA", "TTDB" }));
+
+                    writer.WriteStartElement("to_account");
+                    WriteElementIfNotEmpty(writer, "institution_name", ValidateMinLength(row["Institution Name"], 1));
+                    WriteElementIfNotEmpty(writer, "swift", ValidateMinLength(row["SWIFT"], 1));
+                    WriteElementIfNotEmpty(writer, "account", ValidateString(row["ACCOUNT NUMBER"]));
+                    WriteElementIfNotEmpty(writer, "account_name", ValidateString(row["CUSTOMER NAME"]));
+                    WriteElementIfNotEmpty(writer, "personal_account_type", ValidateString(row["PERSONAL ACCOUNT TYPE"]));
+
+                    writer.WriteStartElement("signatory");
+                    writer.WriteStartElement("t_person");
+                    WriteElementIfNotEmpty(writer, "gender", MapGender(row["GENDER"]?.ToString()));
+                    (firstName, lastName) = SplitName(ValidateString(row["CUSTOMER NAME"]));
+                    WriteElementIfNotEmpty(writer, "first_name", firstName);
+                    WriteElementIfNotEmpty(writer, "last_name", lastName);
+                    WriteElementIfNotEmpty(writer, "birthdate", FormatDate(row["CONDUCTOR DOB"]));
+                    WriteElementIfNotEmpty(writer, "legal_doc_name", ValidateString(row["LEGAL DOC NAME"]));
+                    WriteElementIfNotEmpty(writer, "id_number", ValidateString(row["SIGNATORY PASSPORT ID"]));
+                    WriteElementIfNotEmpty(writer, "occupation", ValidateString(row["SIGNATORY OCCUPATION"]));
+                    writer.WriteEndElement();
+                    writer.WriteEndElement();
+
+                    WriteElementIfNotEmpty(writer, "opened", FormatDate(row["ISSUE DATE"]));
+                    WriteElementIfNotEmpty(writer, "status_code", ValidateString(row["STATUS CODE"]));
+                    writer.WriteEndElement();
+
+                    WriteElementIfNotEmpty(writer, "to_country", ValidateString(row["SIGNATORY NATIONALITY"]));
+                    writer.WriteEndElement();
+                    WriteElementIfNotEmpty(writer, "comments", ValidateString(row["COMMENT"]));
 
                     writer.WriteEndElement();
                 }
 
                 writer.WriteEndElement();
                 writer.WriteEndDocument();
+            }
+            
+        }
+
+        private void WriteElementIfNotEmpty(XmlWriter writer, string tagName, string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                writer.WriteElementString(tagName, value);
+            }
+        }
+
+        private string ValidateString(object value)
+        {
+            return value?.ToString().Trim() ?? "";
+        }
+
+        private string FormatDate(object dateValue)
+        {
+            if (dateValue == null || string.IsNullOrWhiteSpace(dateValue.ToString()))
+                return "";
+            if (DateTime.TryParse(dateValue.ToString(), out DateTime date))
+                return date.ToString("yyyy-MM-ddTHH:mm:ss");
+            return "";
+        }
+
+        private string ValidateEnumeration(object value, string[] validValues)
+        {
+            string strValue = value?.ToString().Trim() ?? "";
+            return validValues.Contains(strValue) ? strValue : "";
+        }
+
+        private string ValidateMinLength(object value, int minLength)
+        {
+            string strValue = value?.ToString().Trim() ?? "";
+            return strValue.Length >= minLength ? strValue : "";
+        }
+
+        private string MapGender(string gender)
+        {
+            if (string.IsNullOrWhiteSpace(gender))
+                return "";
+            return gender.ToLower() == "male" ? "M" : gender.ToLower() == "female" ? "F" : "";
+        }
+
+        private string FormatDescription(string description)
+        {
+            if (string.IsNullOrWhiteSpace(description)) return "";
+            description = description.ToLower();
+            if (description.Contains("withdrawal"))
+                return "cash withdrawal";
+            else if (description.Contains("deposit"))
+                return "cash deposit";
+            return description;
+        }
+
+        private (string firstName, string lastName) SplitName(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName)) return ("", "");
+            var names = fullName.Split(' ');
+            return (names.FirstOrDefault() ?? "", names.Length > 1 ? string.Join(" ", names.Skip(1)) : "");
+        }
+        private void LogError(string message, Exception ex = null)
+        {
+            string logFilePath = Path.Combine(Application.StartupPath, "error_log.txt");
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine("----- Error Log -----");
+                writer.WriteLine("Date/Time: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                writer.WriteLine("Message: " + message);
+                if (ex != null)
+                {
+                    writer.WriteLine("Exception: " + ex.Message);
+                    writer.WriteLine("Stack Trace: " + ex.StackTrace);
+                }
+                writer.WriteLine("---------------------");
+                writer.WriteLine();
             }
         }
     }
